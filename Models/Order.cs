45a90614c3230e73c6392862799e1b15a416ad84
Dylan Moore <dylankmoore@gmail.com>
2024-04-06 14:21:29 -0500

    project setup complete

---
+++ Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Microsoft.EntityFrameworkCore;
+
+namespace HHPW.Models
+{
+    public class Order
+    {
+        public int OrderId {  get; set; }
+        public bool IsOpen { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string Email { get; set; }
+        public bool IsPhone { get; set; }
+        public int UserId { get; set; }
+        public DateTime OrderTime { get; set; }
+        public DateTime? CloseTime { get; set; }
+        public int RevTotal { get; set; }
+        public int Tip {  get; set; }
+        public string PaymentType { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+    }
+}
